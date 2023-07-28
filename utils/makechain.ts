77ed014e29a37ba84b50045323dd280efd61ca20
oyoops <121578862+oyoops@users.noreply.github.com>
2023-07-28 11:44:21 -0400

    Add files via upload

---
+++ utils/makechain.ts
@@ -0,0 +1,36 @@
+import { OpenAI } from 'langchain/llms/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { ConversationalRetrievalQAChain } from 'langchain/chains';
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `You are a helpful AI assistant. You are an expert on the new legislation that just passed in Florida called the Live Local Act (SB-102). Use the following pieces of context from the legislative text to answer the question at the end.
+If you don't know the answer, just say you don't know. Try your best not to make up answers.
+
+{context}
+
+Question: {question}
+Helpful answer in markdown:`;
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0.2, // increase temepreature to get more creative answers
+    modelName: 'gpt-4', //change this to gpt-4 if you have access
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+    },
+  );
+  return chain;
+};
