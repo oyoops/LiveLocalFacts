579528fd63626a460191c0984ea5a7aec358ca8c
oyoops <121578862+oyoops@users.noreply.github.com>
2023-07-28 17:35:08 -0400

    pee

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,10 +8,10 @@ export default function Layout({ children }: LayoutProps) {
       <header className="container sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6 space-x-4"> {/* Added space-x-4 for spacing between links */}
-            <a href="#" className="hover:text-slate-600 cursor-pointer">
+            <a href="https://oyoops.com" className="hover:text-slate-600 cursor-pointer">
               Home
             </a>
-            <a href="https://twitter.com/your_username" className="hover:text-slate-600 cursor-pointer">
+            <a href="https://twitter.com/oyoops" className="hover:text-slate-600 cursor-pointer">
               Twitter
             </a>
             <a href="https://munigpt.xyz" className="hover:text-slate-600 cursor-pointer">
