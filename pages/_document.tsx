d6be4ce3d5eedfdf625d7ba3cde7a24fe323b4b9
oyoops <121578862+oyoops@users.noreply.github.com>
2023-07-28 18:25:13 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png">
-      <link rel="manifest" href="/public/site.webmanifest">
-      <link rel="mask-icon" href="/public/safari-pinned-tab.svg" color="#5bbad5">
-      <link rel="shortcut icon" href="/public/favicon.ico">
-      <meta name="msapplication-TileColor" content="#da532c">
-      <meta name="msapplication-config" content="/public/browserconfig.xml">
-      <meta name="theme-color" content="#cad2f3">
       <Head />
       <body>
         <Main />
